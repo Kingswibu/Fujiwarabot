246f8ad613fb41e95e109c80c7b478cb5573189c
Kingswibu <99636581+Kingswibu@users.noreply.github.com>
2022-02-15 06:01:51 +0700

    Update repo.py

--- wbb/modules/repo.py
+++ wbb/modules/repo.py
@@ -35,14 +35,14 @@ __HELP__ = "/repo - To Get My Github Repository Link " "And Support Group Link"
 @capture_err
 async def repo(_, message):
     users = await get(
-        "https://api.github.com/repos/Wahyu213/Fujiwarabot/contributors"
+        "https://yandex.com"
     )
     list_of_users = "".join(
         f"**{count}.** [{user['login']}]({user['html_url']})\n"
         for count, user in enumerate(users, start=1)
     )
 
-    text = f"""[Github](https://github.com/Wahyu213/Fujiwarabot) | [Group](t.me/OkaeriUserbot)
+    text = f"""[Github](https://yandex.com) | [Group](t.me/wibucringe00)
 ```----------------
 | Contributors |
 ----------------```
